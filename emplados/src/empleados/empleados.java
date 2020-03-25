6a7209e9d78a0d50f638b7f877a46cee2f9974e3
sergiovill1 <sergioconsejo2@gmail.com>
2020-03-24 23:15:23 -0500

    main

--- emplados/src/empleados/empleados.java
+++ emplados/src/empleados/empleados.java
@@ -28,8 +28,44 @@ public class empleados {
                 
             }
             
+            System.out.println('\n');
+            
+            rs = stm.executeQuery("SELECT empresa.nombre, empleado.nombre, empleado.ap_paterno, empleado.ap_materno, empleado.edad FROM empleado , empresa WHERE empleado.id_empresa = empresa.id_empresa AND empleado.edad LIKE '55'");
+
+            
+            while (rs.next()) {
+                String nombre_empresa = rs.getString(1);
+                String id_empresa = rs.getString(2);
+                String ap_paterno = rs.getString(3);
+                String ap_materno = rs.getString(4);
+                int edad = rs.getInt(5);
+                
+                System.out.println(nombre_empresa + " - " + id_empresa + " - " + ap_paterno + " - " + ap_materno + " - " + edad );
+                
+            }
+            
+            System.out.println('\n');
+            
+ rs = stm.executeQuery("SELECT empresa.nombre, empleado.nombre, empleado.ap_paterno, empleado.ap_materno, empleado.edad FROM empleado , empresa WHERE empleado.id_empresa = empresa.id_empresa AND empresa.nombre LIKE 'empresa_1'");
+
+            
+            while (rs.next()) {
+                String nombre_empresa = rs.getString(1);
+                String id_empresa = rs.getString(2);
+                String ap_paterno = rs.getString(3);
+                String ap_materno = rs.getString(4);
+                int edad = rs.getInt(5);
+                
+                System.out.println(nombre_empresa + " - " + id_empresa + " - " + ap_paterno + " - " + ap_materno + " - " + edad );
+                
+            }
+            
+            System.out.println('\n');
+            
         } catch (SQLException e) {
             e.printStackTrace();
+            
+            
             
         } finally {
             try {
